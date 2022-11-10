0c2f5bb2255ce69c322c19942d2498701a4017fa
Matthew Bavier <baviermatthew@gmail.com>
2022-11-09 20:32:54 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import createEngine, {
 } from '@projectstorm/react-diagrams';
 
 import {
-  CanvasWidget
+  CanvasWidget,
 } from '@projectstorm/react-canvas-core';
 
 import Select from 'react-select';
@@ -56,7 +56,7 @@ function process(allText) {
   console.log(partOptions)
 }
 
-
+var selectedNodeX, selectedNodeY, selectedNodeWidth, selectedNodeHeight, selectedNode;
 function addNewNode(engine, partName) {
   let numOfNodes = Object.keys(engine.getModel().getActiveNodeLayer().getModels()).length;
   let node = new DefaultNodeModel({
@@ -67,15 +67,82 @@ function addNewNode(engine, partName) {
   node.addInPort("In");
   node.addOutPort("Out");
   engine.getModel().addNode(node);
+  node.registerListener({
+    selectionChanged: (e) => {
+      if (e.isSelected) {
+        $("div.infoDiv").css("display", "block");
+        console.log(node);
+        Object.keys(dictOfParts[partName]).map((block) => $("div.infoDiv").append("<p>" + block + ": " + dictOfParts[partName][block] + "</p>"));
+        selectedNodeX = node.position.x;
+        selectedNodeY = node.position.y;
+        selectedNodeWidth = node.width;
+        selectedNodeHeight = node.height;
+        selectedNode = node;
+      } else{
+        $("div.infoDiv").css("display", "none");
+        $("div.infoDiv").html("");
+      }
+    },
+
+  })
   engine.repaintCanvas()
+}
+
+var engine;
+
+var lastClick;
+
+function addNewPort(type, name) {
+  if (name != "") {
+    if (type === "output") {
+      selectedNode.addOutPort(name);
+    } else {
+      selectedNode.addInPort(name);
+    }
+    engine.repaintCanvas();
+  }
+  $("div.newPortDiv").css("display","none");
+  $("input#portInput")[0].value = null;
+}
+
+function removeLinks(currentModel, link) {
+  currentModel.removeLink(link)
+}
+
+function deleteNode() {
+  let currentModel = engine.getModel();
+  let inPort = selectedNode.getInPorts();
+  let outPort = selectedNode.getOutPorts();
+  inPort.map((port) => {
+    Object.keys(port.links).map((link) => {currentModel.removeLink(link)})});
+  outPort.map((port) => {
+    Object.keys(port.links).map((link) => {currentModel.removeLink(link)})});
+  currentModel.removeNode(selectedNode);
+  engine.repaintCanvas();
+  $("div.newPortDiv").css("display","none");
+  $("div.infoDiv").css("display", "none");
+  $("div.infoDiv").html("");
+}
+
+
+function handleClick(e) {
+  if (e.clientX > selectedNodeX && e.clientX < selectedNodeX + selectedNodeWidth) {
+    if (e.clientY > selectedNodeY && e.clientY < selectedNodeY + selectedNodeHeight) {
+      if (e.timeStamp - lastClick < 200) {
+        $("div.newPortDiv").css("display","block");
+      }
+    }
+  }
+  lastClick = e.timeStamp;
 }
 
 export default () => {
 
   
   //1) setup the diagram engine
-  var engine = createEngine();
-
+  engine = createEngine({
+    registerDefaultDeleteItemsAction: false
+  });
   //2) setup the diagram model
   var model = new DiagramModel();
 
@@ -93,9 +160,19 @@ export default () => {
         }}
           
         />
-    <div id='containerDiv' style={{zIndex:'-1', position:'absolute', left:0, top:0}}>
+    <div className="newPortDiv">
+        <div className="newPortText">Add a new port:</div>
+        <input id="portInput" type="text" placeholder="Node Name"></input>
+        <div className="buttonDiv">
+          <button className="portButton" onClick={() => addNewPort("input", document.getElementById("portInput").value)}> Input </button>
+          <button className="portButton" onClick={() => addNewPort("output", document.getElementById("portInput").value)}> Output </button>
+          <button className="portButton" onClick={() => deleteNode()}> Delete </button>
+        </div>
+    </div>
+    <div id='containerDiv' style={{zIndex:'-1', position:'absolute', left:0, top:0}} onMouseDown={(e) => handleClick(e)} >
       <CanvasWidget engine={engine} />
       </div>
+    <div className='infoDiv'></div>
     </div>
   );
 };
