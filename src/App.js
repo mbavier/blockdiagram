4b726eb1758988ee1a2a937c07e07f94d069e457
Matthew Bavier <baviermatthew@gmail.com>
2022-11-20 11:59:36 -0800

    Added CSV upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+<<<<<<< Updated upstream
 import React from "react";
+=======
+import React, {useRef} from "react";
+import Select from 'react-select';
+
+import { defaultDictOfParts, defaultPartOptions } from "./data";
+>>>>>>> Stashed changes
 import createEngine, { 
   /*DefaultLinkModel, */
   DefaultNodeModel,
@@ -15,6 +22,54 @@ import $ from 'jquery';
 import { dictOfParts, partOptions } from "./data";
 
 import './App.css';
+// $(document).ready(function() {
+//     $("#file").on('change', function() {
+//       let reader = new FileReader();
+//       console.log($(reader.readAsText("#file").prop('files')[0]));
+//     })
+//   });
+  
+function handleFileSelect(e, setDictOfParts, setPartOptions) {
+  let file = e.target.files;
+  let f = file[0];
+
+  let reader = new FileReader();
+
+  reader.onload = (function(e) {
+    process(e.currentTarget.result, setDictOfParts, setPartOptions);
+  });
+    
+
+  reader.readAsText(f);
+}
+  
+  
+  
+  
+  
+  
+  function process(allText, setDictOfParts, setPartOptions) {
+    let newDictOfParts = {};
+    let newPartOptions = [];
+    var allTextLines = allText.split('\r\n')
+    var headers = allTextLines[0].split(',');
+    console.log(allTextLines, headers);
+    for (var i=1; i<allTextLines.length; i++) {
+      console.log(i);
+      var data = allTextLines[i].split(',');
+          if (data.length == headers.length) {
+            newDictOfParts[data[0]] = {}
+            newPartOptions[i] = {value: data[0], label: data[0]}
+              for (var j=1; j<headers.length; j++) {
+                console.log(data[0], data[j])
+                newDictOfParts[data[0]][headers[j]] = data[j];
+              }
+          }
+  
+    }
+    setDictOfParts(newDictOfParts);
+    setPartOptions(newPartOptions)
+  }
 
 const customStyles = {
   menu: (provided) => ({
@@ -181,3 +236,45 @@ export default () => {
     </div>
   );
 };
+<<<<<<< Updated upstream
+=======
+
+const customStyles = {
+  menu: (provided) => ({
+    ...provided,
+    zIndex: 100
+  })
+}
+
+var selectedPart;
+function handleChange(selectedOption, partInfo)  {
+  selectedPart = [selectedOption['value'], partInfo];
+}
+
+
+
+function PartSelect() {
+  var [dictOfParts, setDictOfParts] = React.useState(defaultDictOfParts);
+  var [partOptions, setPartOptions] = React.useState(defaultPartOptions);
+  const inputFile = useRef(null);
+
+  const onInputBtnClick = () => {
+    inputFile.current.click();
+  }
+
+  return (
+    <div>
+        <input type="file" id="file" ref={inputFile} style={{display:"none"}} onChange={(e) => {handleFileSelect(e, setDictOfParts, setPartOptions)}}/>
+         
+  <Select className="partInput" defaultValue={partOptions[0]} options={partOptions} styles={customStyles} onChange={(e) => {handleChange(e, dictOfParts[e.value])}} onKeyDown={(e) => {
+    if (e.key === 'Enter') {
+        console.log(selectedPart)
+        addNewNode(engine, selectedPart[0], selectedPart[1]);
+    }
+  }}
+  />
+  <button onClick={onInputBtnClick}> Change input</button>
+   </div>
+  );
+}
+>>>>>>> Stashed changes
